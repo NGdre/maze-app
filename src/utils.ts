50a0ae5abdbdc0f83aca578a12d1410108c6d345
NGdre <100stepstosuccess@gmail.com>
2025-08-08 12:43:18 +0300

    add a helper to transfrom all values of generator functions

--- src/utils.ts
+++ src/utils.ts
@@ -22,3 +22,40 @@ export function loopPairs<T>(pairs: Array<T>, cb: (prev: T, curr: T) => void) {
     prev = curr;
   }
 }
+
+export function mapGenerator<T, U, R>(
+  generator: Generator<T, R>,
+  transform: (value: T) => U
+): Generator<U, R> {
+  return {
+    next(arg?: unknown): IteratorResult<U, R> {
+      const result = generator.next(arg);
+      return result.done
+        ? { value: result.value, done: true }
+        : { value: transform(result.value), done: false };
+    },
+
+    return(value?: unknown): IteratorResult<U, R> {
+      if (generator.return) {
+        const result = generator.return(value as R);
+        return result.done
+          ? { value: result.value, done: true }
+          : { value: transform(result.value), done: false };
+      } else {
+        return { value: value as R, done: true };
+      }
+    },
+
+    throw(error?: unknown): IteratorResult<U, R> {
+      if (!generator.throw) throw error;
+      const result = generator.throw(error);
+      return result.done
+        ? { value: result.value, done: true }
+        : { value: transform(result.value), done: false };
+    },
+
+    [Symbol.iterator]() {
+      return this;
+    },
+  };
+}
