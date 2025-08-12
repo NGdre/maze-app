fe49fff672a36fa4988ec012fc1ffaf0870b451e
NGdre <100stepstosuccess@gmail.com>
2025-08-12 17:01:36 +0300

    [WIP]: make constant objects readonly
    no one should be able to change constant objects

--- src/constants.ts
+++ src/constants.ts
@@ -9,20 +9,20 @@ export const MAX_COLUMNS = 100;
 export const PROJECT_NAME = "maze app";
 
 export const colors = {
-  WALL_COLOR: "black",
+  WALL_COLOR: "#5f0f40",
   EMPTY_CELL: "white",
   START_CELL: "green",
   END_CELL: "pink",
   HOVERED_CELL: "purple",
   PATH_COLOR: "red",
-};
+} as const;
 
 export const PATH_WIDTH = 2;
 export const WALLS_WIDTH = 0.5;
 
 export const localStorageKeys = {
   MAZE_MATRIX: "maze-matrix",
-};
+} as const;
 
 export const PATH_ANIMATION_DELAY = 0;
 export const CELL_SELECTION_THROTTLE_DELAY = 0;
@@ -33,4 +33,4 @@ export const workTypes = {
   mazeGeneration: "mazeGeneration",
   renderOnly: "renderOnly",
   idle: "idle",
-};
+} as const;
