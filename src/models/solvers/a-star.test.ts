df97298815ffea87fd641b3c8b3f45c3016c6d45
NGdre <100stepstosuccess@gmail.com>
2025-09-14 17:18:33 +0300

    [TEST]: add tests to aStar using fixture

--- src/models/solvers/a-star.test.ts
+++ src/models/solvers/a-star.test.ts
@@ -1,50 +1,102 @@
-import fs from "fast-check";
-import { recursiveBacktracking } from "../generators/recursive-backtracking";
-import {
-  createIdToCellMap,
-  createRectMaze,
-  fillCellsWithOpenNeighbors,
-  generateRectMazeId,
-  removeWallsBetweenCells,
-} from "../maze";
-import { isValidPath } from "./helpers";
-import aStar from "./a-star";
+import { aStarVisualSchema } from "src/configs/visual";
+import { generateRectMazeId, type PolygonCell } from "../maze";
+import { applyAStarVisual, aStarSerial } from "./a-star";
+import { idToCellMapFixture } from "./fixtures/id-to-cell-map-fixture";
+
+jest.mock("./reconstruct-path", () => ({
+  __esModule: true,
+  reconstructPathSerial: jest.fn(function* () {
+    yield* ["8,0", "1,1", "2,3"];
+  }),
+}));
 
 describe("aStar", () => {
-  it("works with rect maze and recursive backtracking", () => {
-    const m = 25,
-      n = 15;
+  const m = 5,
+    n = 10;
+  const start = generateRectMazeId(0, 0);
+  const end = generateRectMazeId(m - 1, n - 1);
 
-    const edgeLength = 20;
+  it("solution steps have the right format", () => {
+    const steps = [
+      ...aStarSerial(
+        start,
+        end,
+        idToCellMapFixture as Map<string, PolygonCell>
+      ),
+    ];
+    const pathSteps = steps.filter((step) => step.isPathCell);
+    const nonPathSteps = steps.filter((step) => !step.isPathCell);
 
-    const maze = createRectMaze(m, n, edgeLength);
-    const cells = maze.cells;
+    const anyAStarCell = {
+      id: expect.any(String),
+      text: expect.objectContaining({
+        "h-value": expect.any(String),
+        "g-value": expect.any(String),
+        "f-value": expect.any(String),
+      }),
+    };
 
-    const toRemove = [...recursiveBacktracking(m, n)].map((pair) => [
-      pair[0].id,
-      pair[1].id,
+    nonPathSteps.forEach((step) => {
+      expect(step).toMatchObject({
+        isPathCell: false,
+        visited: expect.objectContaining(anyAStarCell),
+      });
+
+      expect(Array.isArray(step.enqueued)).toBe(true);
+
+      if (step.enqueued.length > 0) {
+        step.enqueued.forEach((item) => {
+          expect(item).toMatchObject(anyAStarCell);
+        });
+      }
+    });
+
+    pathSteps.forEach((step, i) => {
+      expect(step).toEqual({
+        isPathCell: true,
+        foundPath: ["1,1", "2,3"][i],
+        prevCellId: ["8,0", "1,1"][i],
+      });
+    });
+  });
+});
+
+describe("applyAStarVisual", () => {
+  test("works as expected when isPathCell equals true", () => {
+    const solutionStep = {
+      isPathCell: true,
+      prevCellId: "0,0",
+      foundPath: "0,1",
+    };
+
+    expect(applyAStarVisual(solutionStep)).toEqual([
+      {
+        id: solutionStep.foundPath,
+        color: aStarVisualSchema.foundPath.colors.background,
+        lineColor: aStarVisualSchema.foundPath.colors.line,
+        prevCellId: solutionStep.prevCellId,
+        isPathCell: true,
+      },
     ]);
+  });
 
-    // @ts-ignore
-    removeWallsBetweenCells(cells, toRemove);
-    fillCellsWithOpenNeighbors(cells);
-    const idToCellMap = createIdToCellMap(cells);
+  test("when isPathCell equals false and there are no cells for enqueued array", () => {
+    const solutionStep = {
+      isPathCell: false,
+      visited: {
+        id: "0,0",
+        text: {},
+      },
+      enqueued: [],
+    };
 
-    fs.assert(
-      fs.property(
-        fs.nat(m - 1),
-        fs.nat(n - 1),
-        fs.nat(m - 1),
-        fs.nat(n - 1),
-        (s1, s2, e1, e2) => {
-          const start = generateRectMazeId(s1, s2);
-          const end = generateRectMazeId(e1, e2);
-
-          const path = aStar(start, end, idToCellMap);
-
-          if (path) expect(isValidPath(cells, start, end, path)).toBe(true);
-        }
-      )
-    );
+    expect(applyAStarVisual(solutionStep)).toEqual([
+      {
+        id: solutionStep.visited.id,
+        text: solutionStep.visited.text,
+        color: aStarVisualSchema.visited.colors.background,
+        isPathCell: false,
+      },
+    ]);
   });
 });
